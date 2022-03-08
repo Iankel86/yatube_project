3fa389419996d064efbdddb8c914866e85a1b473
Ян Шадрин <Yankelll86@mail.ru>
2022-03-08 19:18:58 +0300

    main

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 # Главная страница
 def index(request):    
-    return HttpResponse('Главная страница')
+#    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 # Страница с постами
 def group_posts(request, slug):
