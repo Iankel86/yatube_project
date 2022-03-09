ff7c8c77bd3c3bcd989ae29bef40f9faeb36bc08
Ян Шадрин <Yankelll86@mail.ru>
2022-03-09 23:18:12 +0300

    откорректировал папки

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
 # Главная страница
-def index(request):    
+
+
+def index(request):
 #    return HttpResponse('Главная страница')
     template = 'posts/index.html'
     return render(request, template)
 
 # Страница с постами
+
+
 def group_posts(request, slug):
     return HttpResponse('Страница с постом после фильтра по группе')
