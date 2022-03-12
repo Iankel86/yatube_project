99dc4d65b60a50f8bb4e53ebbdadef6d39f823f1
Ян Шадрин <Yankelll86@mail.ru>
2022-03-12 20:09:02 +0300

    активизировал логотип

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,7 +7,7 @@ app_name = 'posts'
 
 urlpatterns = [
     # Главная страница
-    path('', views.index),
-    path('group_list/', views.group_list),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('group_list/', views.group_list, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
 ]
